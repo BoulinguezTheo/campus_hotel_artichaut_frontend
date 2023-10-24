f71a5e822860e155044b0876d26337badd5f5a4a
Theo <119578352+BoulinguezTheo@users.noreply.github.com>
2023-10-24 18:50:29 +0200

    add scaled size librairy

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:campus_hotel_artichaut_frontend/theme/theme.dart';
 import 'package:campus_hotel_artichaut_frontend/utils/constants.dart';
-import 'package:campus_hotel_artichaut_frontend/views/account.dart';
-import 'package:campus_hotel_artichaut_frontend/views/homepage.dart';
+import 'package:campus_hotel_artichaut_frontend/views/main_pages/account.dart';
+import 'package:campus_hotel_artichaut_frontend/views/main_pages/homepage.dart';
 import 'package:campus_hotel_artichaut_frontend/views/init_app.dart';
-import 'package:campus_hotel_artichaut_frontend/views/rooms.dart';
+import 'package:campus_hotel_artichaut_frontend/views/main_pages/rooms.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:scaled_size/scaled_size.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
-GlobalKey<NavigatorState>(debugLabel: 'root');
+    GlobalKey<NavigatorState>(debugLabel: 'root');
 final GlobalKey<NavigatorState> _shellNavigatorKey =
-GlobalKey<NavigatorState>(debugLabel: 'shell');
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 void main() {
   runApp(MyApp());
@@ -20,38 +21,40 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final GoRouter _router = GoRouter(
-    navigatorKey: _rootNavigatorKey,
-    initialLocation: ConstantsApp.HOMEPAGE_ROUTE,
-    routes: <RouteBase>[
-    ShellRoute(
-      navigatorKey: _shellNavigatorKey,
-      builder: (context, state, child) {
-        return InitApp(child: child);
-      },
-      routes: [
-        GoRoute(
-          path: ConstantsApp.HOMEPAGE_ROUTE,
-          builder: (context, state) => const MyHomepage(),
-        ),
-        GoRoute(
-          path: ConstantsApp.ACCOUNT_ROUTE,
-          builder: (context, state) => const MyAccount(),
-        ),
-        GoRoute(
-          path: ConstantsApp.ROOM_LIST_ROUTE,
-          builder: (context, state) => const MyRooms(),
-        ),
-      ],
-    )
-  ]);
+      navigatorKey: _rootNavigatorKey,
+      initialLocation: ConstantsApp.HOMEPAGE_ROUTE,
+      routes: <RouteBase>[
+        ShellRoute(
+          navigatorKey: _shellNavigatorKey,
+          builder: (context, state, child) {
+            return InitApp(child: child);
+          },
+          routes: [
+            GoRoute(
+              path: ConstantsApp.HOMEPAGE_ROUTE,
+              builder: (context, state) => const MyHomepage(),
+            ),
+            GoRoute(
+              path: ConstantsApp.ACCOUNT_ROUTE,
+              builder: (context, state) => const MyAccount(),
+            ),
+            GoRoute(
+              path: ConstantsApp.ROOM_LIST_ROUTE,
+              builder: (context, state) => const MyRooms(),
+            ),
+          ],
+        )
+      ]);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: myTheme,
-      routerConfig: _router,
-    );
+    return ScaledSize(builder: () {
+      return MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: myTheme,
+        routerConfig: _router,
+      );
+    });
   }
 }
---
+++ lib/views/homepage_widgets/hero.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:scaled_size/scaled_size.dart';
+
+class Hero extends StatelessWidget {
+  const Hero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 75.vh,
+    );
+  }
+}
